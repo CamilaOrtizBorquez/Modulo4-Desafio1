2006addf764693e6cc11eb51ca59acb52e2e2a70
Camila Ortiz <ortiz.camilafernanda@gmail.com>
2024-08-05 23:06:56 -0400

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import { Navbar } from './component/Navbar'
+import { Home } from './component/Home'
+import { Footer } from './component/Footer'
+
+function App() {
+ 
+  return (
+    <>
+      <Navbar></Navbar>
+      <Home></Home>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
